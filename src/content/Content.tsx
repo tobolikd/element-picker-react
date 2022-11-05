13bff2d2864b49474d7fe8444fb4443005d4e1a3
David Tobolik <tobolikg@gmail.com>
2022-11-05 12:44:00 +0100

    new skill: adding element

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -1,10 +1,20 @@
 import styles from './Content.module.css'
+import {readingElement} from "../picker/picker"
+
 
 const Content = () => {
+	const get_element = () => {
+		// get element under cursor
+		var cont = document.getElementById('smartlook-element-info');
+		if (cont) readingElement(cont);
+	};
+	
 	return (
+
 		<div id={'smartlook-element-picker'} className={styles.content}>
 			<h3>Current element:</h3>
 			<p id={'smartlook-element-info'}>No element hovered yet</p>
+			<button onClick={get_element}>Pick element</button>
 		</div>
 	)
 }
