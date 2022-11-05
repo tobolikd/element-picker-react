9223f91b706b1e3f92c34ee8cbadce1b00bafdfe
David Tobolik <tobolikg@gmail.com>
2022-11-05 14:35:50 +0100

    feat: preventing defsult event behaviour

--- src/picker/picker.tsx
+++ src/picker/picker.tsx
@@ -7,17 +7,15 @@ export function readingElement(elem: HTMLElement, doc: Document, win: Window) {
     let selected : string = "";
     var current = "";
 
-    
     doc.addEventListener('click', clickEvent => {
+        clickEvent.preventDefault();
         selected += "<p>" + doc.elementFromPoint(clickEvent.clientX, clickEvent.clientY) + "</p>";
-    }, {passive: true})
+    })
     
     doc.addEventListener('mousemove', hoverEvent => {
         current = "<p>" + doc.elementFromPoint(hoverEvent.clientX, hoverEvent.clientY) + "</p>";
         elem.innerHTML = selected + current;
     }, {passive: true})
-
-
 }
 
 export function stopReadingElement(elem: HTMLElement, doc: Document, win: Window) {
