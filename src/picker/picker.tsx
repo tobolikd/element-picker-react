35965e4a273e27fd53f872c14456015b1ad15d00
David Tobolik <tobolikg@gmail.com>
2022-11-05 12:53:15 +0100

    sorry guys

---
+++ src/picker/picker.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// main picker code
+export function readingElement(elem: HTMLElement) {
+    var a = "<p>FUCK YOU</p>"
+    elem.innerHTML = a;
+}
+
+module.exports = {
+    readingElement
+}
